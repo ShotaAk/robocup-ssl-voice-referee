355231916490bded2c89fb75feb49594eafa48d4
ShotaAk <macakasit@gmail.com>
2023-04-14 06:15:18 +0900

    Refactoring text_to_voice

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 import argparse
 import time
 
-from text_to_voice import text_to_voice
+from text_to_voice import TextToVoice 
 from speech_script import load_speech_scripts
 from referee_receiver import RefereeReceiver
 
 
-def main(ref_receiver, speech_scripts):
+def main(speech_scripts):
     prev_command_count = -1
     while True:
         referee_msg = ref_receiver.get_referee_message()
@@ -19,10 +19,10 @@ def main(ref_receiver, speech_scripts):
             command = referee_msg.command
 
             if command in speech_scripts.keys():
+                text_to_voice.stop()
                 for text in speech_scripts[command].get_texts():
                     print("Play text: {}".format(text))
-                    text_to_voice(text)
-                    time.sleep(1)  # 休みなく読み上げることを防ぐ
+                    text_to_voice.play(text)
                     
             else:
                 print('コマンド:{}に対応した原稿がありません'.format(command))
@@ -42,22 +42,23 @@ if __name__ == "__main__":
         )
     args = parser.parse_args()
 
+    ref_receiver = RefereeReceiver(args.addr, args.port)
+    speech_scripts = load_speech_scripts()
+    text_to_voice = TextToVoice()
+
     print("Start Robocup SSL Voice Referee")
     print("    Usage: Ctrl-C to exit\n")
 
     print("Check VOICEVOX connection...")
-    if text_to_voice(""):
+    if text_to_voice.play(""):
         print("    OK\n")
     else:
         print("Exit.")
         exit(1)
 
-    ref_receiver = RefereeReceiver(args.addr, args.port)
-    speech_scripts = load_speech_scripts()
-
     print("Waiting for referee command...")
     try:
-        main(ref_receiver, speech_scripts)
+        main(speech_scripts)
     except KeyboardInterrupt:
         pass
 
--- text_to_voice.py
+++ text_to_voice.py
@@ -4,38 +4,59 @@ import requests
 import simpleaudio
 import urllib.parse
 
-def text_to_voice(text="テストです", speaker_id=1, speed_scale=1.2, volume_scale=1.0):
-    # クエリ取得
-    url = "http://localhost:50021/audio_query?speaker=" \
-        + str(speaker_id) \
-        + "&text=" + urllib.parse.quote(text)
-    query = None
-    try:
-        response = requests.post(url)
-        response.raise_for_status()
-        query = response.json()
-    except requests.exceptions.RequestException as e:
+class TextToVoice():
+    def __init__(self):
+        self._playing_obj = None
+
+    def _get_query(self, text, speaker_id):
+        success = True
+        query = None
+
+        url = "http://localhost:50021/audio_query?speaker=" \
+            + str(speaker_id) \
+            + "&text=" + urllib.parse.quote(text)
+        try:
+            response = requests.post(url)
+            response.raise_for_status()
+            query = response.json()
+        except requests.exceptions.RequestException as e:
             print("    Error: can not get query because:{}".format(e))
-            return False
+            success = False
 
-    # クエリ加工
-    query['speedScale'] = speed_scale
-    query['volumeScale'] = volume_scale
+        return success, query
 
-    # wav取得
-    url = "http://localhost:50021/synthesis?speaker=" + str(speaker_id)
-    file_name = 'output.wav'
-    wav_path = Path(file_name)
-    try:
-        response = requests.post(url, json=query)
-        response.raise_for_status()
-        wav_path.write_bytes(response.content)
-    except requests.exceptions.RequestException as e:
+    def _generate_wav_file(self, file_path, query, speaker_id):
+        success = True
+
+        url = "http://localhost:50021/synthesis?speaker=" + str(speaker_id)
+        wav_path = Path(file_path)
+        try:
+            response = requests.post(url, json=query)
+            response.raise_for_status()
+            wav_path.write_bytes(response.content)
+        except requests.exceptions.RequestException as e:
             print("    Error: can not get wav data because:{}".format(e))
+            success = False
+        
+        return success
+
+    def play(self, text="", speaker_id=1, speed_scale=1.2, volume_scale=1.0):
+        success, query = self._get_query(text, speaker_id)
+        if not success:
             return False
 
-    # 再生
-    wav_obj = simpleaudio.WaveObject.from_wave_file(file_name)
-    play_obj = wav_obj.play()
-    play_obj.wait_done()
-    return True
+        query['speedScale'] = speed_scale
+        query['volumeScale'] = volume_scale
+
+        file_path = 'output.wav'
+        if not self._generate_wav_file(file_path, query, speaker_id):
+            return False
+
+        wav_obj = simpleaudio.WaveObject.from_wave_file(file_path)
+        self._playing_obj = wav_obj.play()
+        return True
+
+    def stop(self):
+        if self._playing_obj:
+            self._playing_obj.stop()
+    
